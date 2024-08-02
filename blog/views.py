70ab8a4a3351b48475b46a13aa58b3d63979c9a3
Vinicius Lucio <vini_lucio_oliveira@hotmail.com>
2024-08-02 16:59:16 -0300

    Posts

--- blog/views.py
+++ blog/views.py
@@ -15,9 +15,30 @@ def index(request):
     return render(request, 'blog/pages/index.html', {'page_obj': page_obj, })
 
 
-def page(request):
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)  # type:ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/pages/index.html', {'page_obj': page_obj, })
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)  # type:ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/pages/index.html', {'page_obj': page_obj, })
+
+
+def page(request, slug):
     return render(request, 'blog/pages/page.html')
 
 
-def post(request):
-    return render(request, 'blog/pages/posts.html')
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()  # type:ignore
+    return render(request, 'blog/pages/posts.html', {'post': post})
