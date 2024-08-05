8649129ead0ac2028aea2d6e1603e4d740cd9059
Vinicius Lucio <vini_lucio_oliveira@hotmail.com>
2024-08-05 12:14:46 -0300

    Search configurado

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -43,6 +44,24 @@ def tag(request, slug):
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'blog/pages/index.html', {'page_obj': page_obj, })
+
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = (
+        Post.objects.get_published()  # type:ignore
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    # paginator = Paginator(posts, PER_PAGE)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/pages/index.html', {'page_obj': posts, 'search_value': search_value})
 
 
 def page(request, slug):
