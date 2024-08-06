cf1c66147a7241660d8dcc2b6f4b7fb721cc6689
Vinicius Lucio <vini_lucio_oliveira@hotmail.com>
2024-08-06 14:58:53 -0300

    Detail View do Pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import (PostListView, post, page, CreatedByListView,
+from blog.views import (PostListView, post, PageDetailView, CreatedByListView,
                         CategoryListView, TagListView, SearchListView)
 
 app_name = 'blog'
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404, HttpRequest, HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 PER_PAGE = 9
 
@@ -206,15 +206,32 @@ class SearchListView(PostListView):
 #                                                      'page_title': page_title, })
 
 
-def page(request, slug):
-    page_obj = Page.objects.filter(is_published=True).filter(slug=slug).first()
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
 
-    if page_obj is None:
-        raise Http404
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        page = self.get_object()
+        page_title = f'{page.title} - '  # type:ignore
+        context.update({'page_title': page_title, })
+        return context
 
-    page_title = f'{page_obj.title} - '
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
 
-    return render(request, 'blog/pages/page.html', {'page': page_obj, 'page_title': page_title, })
+
+# def page(request, slug):
+#     page_obj = Page.objects.filter(is_published=True).filter(slug=slug).first()
+
+#     if page_obj is None:
+#         raise Http404
+
+#     page_title = f'{page_obj.title} - '
+
+#     return render(request, 'blog/pages/page.html', {'page': page_obj, 'page_title': page_title, })
 
 
 def post(request, slug):
